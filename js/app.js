4dace83e2d94ddcbdf9036df91a6816aee118386
jospin <lucien.carbonare@gmail.com>
2015-02-04 17:05:32 -0200

    Integrando com o firebase para realizar o login

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
-angular.module('ionicApp', ['ionic'])
 
-.config(function($stateProvider, $urlRouterProvider) {
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+
+
+var myApp = angular.module('starter', ['ionic', 'firebase'])
+
+myApp.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-    .state('signin', {
-      url: '/sign-in',
-      templateUrl: 'templates/sign-in.html',
-      controller: 'SignInCtrl'
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
     })
-
-    // .state('forgotpassword', {
-    //   url: '/forgot-password',
-    //   templateUrl: 'templates/forgot-password.html'
-    // })
 
     .state('tabs', {
       url: '/tab',
@@ -49,18 +65,48 @@ angular.module('ionicApp', ['ionic'])
     });
 
 
-   $urlRouterProvider.otherwise('/sign-in');
+   $urlRouterProvider.otherwise('/login');
 
-})
+});
 
-.controller('SignInCtrl', function($scope, $state) {
+myApp.controller("loginCtrl", function($scope, $rootScope, $firebase, $firebaseSimpleLogin) {
+    // Get a reference to the Firebase
+    var firebaseRef = new Firebase("https://mt4cisco.firebaseio.com/");
 
-  $scope.signIn = function(user) {
 
-    $state.go('tabs.home');
-  };
-})
+      // Create a Firebase Simple Login object
+      $scope.auth = $firebaseSimpleLogin(firebaseRef);
 
+      // Initially set no user to be logged in
+      $scope.user = null;
+
+      // Logs a user in with inputted provider
+      $scope.login = function(provider) {
+        $scope.auth.$login(provider);
+      };
+
+      // Logs a user out
+      $scope.logout = function() {
+        $scope.auth.$logout();
+      };
+
+      // Upon successful login, set the user object
+      $rootScope.$on("$firebaseSimpleLogin:login", function(event, user) {
+        $scope.user = user;
+        console.log($scope.user);
+      });
+
+      // Upon successful logout, reset the user object
+      $rootScope.$on("$firebaseSimpleLogin:logout", function(event) {
+        $scope.user = null;
+      });
+
+      // Log any login-related errors to the console
+      $rootScope.$on("$firebaseSimpleLogin:error", function(event, error) {
+        console.log("Falha no login: ", error);
+      });
+});
+/*
 .controller('HomeCtrl', function($scope, $state) {
 
   $scope.confirm = function(params) {
@@ -71,3 +117,4 @@ angular.module('ionicApp', ['ionic'])
 .controller('HomeTabCtrl', function($scope) {
   console.log('HomeTabCtrl');
 })
+*/
